6a1c1f6edba97f00aa90848e35af5bcb9d7b230e
hezhengwen <13760214010@163.com>
2019-01-31 14:20:42 +0800

    service 增加更新删除方法

--- src/main/java/business/service/MyService.java
+++ src/main/java/business/service/MyService.java
@@ -6,4 +6,10 @@ public class MyService {
     public void inert(Map map) {
         System.out.println("insert " + map);
     }
+    public void update(Map map) {
+        System.out.println("update " + map);
+    }
+    public void delete(Map map) {
+        System.out.println("delete " + map);
+    }
 }
