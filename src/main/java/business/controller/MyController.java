6417927333b920f33c51aee610d7d281fa67aba6
hezhengwen <13760214010@163.com>
2019-01-31 14:17:12 +0800

    增加业务代码

---
+++ src/main/java/business/controller/MyController.java
@@ -0,0 +1,13 @@
+package business.controller;
+
+public class MyController {
+    public String hello(String name) {
+        return "hello," + name;
+    }
+    public String hello2(String name) {
+        return "hello2," + name;
+    }
+    public String hello3(String name) {
+        return "hello3," + name;
+    }
+}
---
+++ src/main/java/business/service/MyService.java
@@ -0,0 +1,9 @@
+package business.service;
+
+import java.util.Map;
+
+public class MyService {
+    public void inert(Map map) {
+        System.out.println("insert " + map);
+    }
+}
