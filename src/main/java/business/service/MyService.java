0454ebec6bc45c2a3d5829bfcb8bd3d92b4bb557
hezhengwen <13760214010@163.com>
2019-01-31 15:59:16 +0800

    1

--- src/main/java/business/service/MyService.java
+++ src/main/java/business/service/MyService.java
@@ -3,18 +3,23 @@ package business.service;
 import java.util.Map;
 
 public class MyService {
+
     public void inert(Map map) {
         System.out.println("insert " + map);
     }
+
     public void update(Map map) {
         System.out.println("update " + map);
     }
+
     public void delete(Map map) {
         System.out.println("delete " + map);
     }
+
     public void findById(String id) {
         System.out.println("findById " + id);
     }
+
     public void findAll(Map map) {
         System.out.println("findAll " + map);
     }
